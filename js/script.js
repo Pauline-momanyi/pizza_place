86271e5ead33a30a72d15e323431c7be5874a137
Pauline-momanyi <paulinemomanyi55@gmail.com>
2022-03-18 19:20:08 +0300

    Add control flow statements for finding cost of crust and toppings

--- js/script.js
+++ js/script.js
@@ -25,6 +25,7 @@ $(document).ready(function(){
         var toppingPrice
         var crustPrice
 
+        //size
         if (asize === "1"){
             sizePrice = 400;
             //console.log(sizePrice); 
@@ -34,6 +35,72 @@ $(document).ready(function(){
             sizePrice = 800;
         }
         console.log("Cost is " + sizePrice);
+
+        //topping
+        if (asize === "1"){
+            if (atopping === "4"){
+                toppingPrice = 80;
+            }else if (atopping === "5"){
+                toppingPrice = 100;
+            }else if (atopping === "6"){
+                toppingPrice = 120;
+            }
+        }
+
+        if (asize === "2"){
+            if (atopping === "4"){
+                toppingPrice = 100;
+            }else if (atopping === "5"){
+                toppingPrice = 120;
+            }else if (atopping === "6"){
+                toppingPrice = 140;
+            }
+        }
+
+        if (asize === "3"){
+            if (atopping === "4"){
+                toppingPrice = 120;
+            }else if (atopping === "5"){
+                toppingPrice = 140;
+            }else if (atopping === "6"){
+                toppingPrice = 160;
+            }
+        }
+        console.log("Topping costs " + toppingPrice);
+
+        //crust
+        if (asize === "1"){
+            if (acrust === "7"){
+                crustPrice = 150;
+            }else if (acrust === "8"){
+                crustPrice = 170;
+            }else if (acrust === "9"){
+                crustPrice = 200;
+            }
+        }
+
+        if (asize === "2"){
+            if (acrust === "7"){
+                crustPrice = 170;
+            }else if (acrust === "8"){
+                crustPrice = 200;
+            }else if (acrust === "9"){
+                crustPrice = 230;
+            }
+        }
+
+        if (asize === "3"){
+            if (acrust === "7"){
+                crustPrice = 200;
+            }else if (acrust === "8"){
+                crustPrice = 230;
+            }else if (acrust === "9"){
+                crustPrice = 250;
+            }
+        }
+        console.log("Crust costs " + crustPrice);
+    
+    
     })
 
     
