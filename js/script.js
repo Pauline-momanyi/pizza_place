d3cf8619a5a44bd8a4ecff43d407964f2f41ea60
Pauline-momanyi <paulinemomanyi55@gmail.com>
2022-03-18 19:05:39 +0300

    Add a control statement to determine price of pizza depending on size

--- js/script.js
+++ js/script.js
@@ -6,11 +6,37 @@ $(document).ready(function(){
         this.crust = crust        
     }
     Order.prototype.fullOrder = function(){
-        return ("Your order has " + this.size + " " + topping + " " + crust); 
+        return ("Your order has " + this.size + " " + this.topping + " " + this.crust); 
     }
 
     //user interface
+    // var asize = $("#size").val();    
+    // var atopping = $("#topping").val();
+    // var acrust = $("#crust").val();
+    $("#ordernow").click(function(e){
+        var asize = $("#size").val();    
+        var atopping = $("#topping").val();
+        var acrust = $("#crust").val();
+        var yourOrder = new Order (asize, atopping, acrust);
+        console.log(yourOrder.fullOrder());
 
+        //pricing
+        var sizePrice
+        var toppingPrice
+        var crustPrice
+
+        if (asize === "1"){
+            sizePrice = 400;
+            //console.log(sizePrice); 
+        }else if (asize === "2"){
+            sizePrice = 600;
+        }else if (asize === "3"){
+            sizePrice = 800;
+        }
+        console.log("Cost is " + sizePrice);
+    })
+
+    
 
 
 
